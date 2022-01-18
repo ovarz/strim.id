eea179124d6010162d6baf9f0acc5cf3dd90e576
ovarz <ovarz_03@yahoo.com>
2022-01-18 10:47:07 +0700

    update search

--- search/result.php
+++ search/result.php
@@ -13,6 +13,22 @@
 <div class="site-container">
   <?php require ($_SERVER['STRIMID'].'module/search-box.php')?>
   
+
+  
+  <section class="section-container section-video">
+    <?php $follow='yes'; require ($_SERVER['STRIMID'].'module/streamer-list.php')?>
+    <div class="section-title">Latest from <?php echo $random_name[array_rand($random_name)]; ?></div>
+    <div class="video-list">
+      <?php for ($i=1; $i <= 5; $i++) { ?>
+        <?php $video_type='vod'; require ($_SERVER['STRIMID'].'module/video-list.php')?>
+      <?php } ?>
+    </div>
+    <a class="profilepage-button btn" href="username/">
+      <span>More Video</span>
+      <svg class="svgicon svgicon-more" width="12" height="12" viewBox="0 0 10 10"><path d="M8.45,9,9.5,10l5-5-5-5L8.45,1.05l3.2,3.2H4.5v1.5h7.15Z" transform="translate(-4.5 0)"></path></svg>
+    </a>
+  </section>
+  
   
   
   <section class="section-container section-video">
